e2461dd05c26a8173fc59563fe0edfefbb01ce8b
Jules Wei <s.jules2000@gmail.com>
2016-04-05 23:36:49 +0000

    added jQuery to betting game edited javascript file

--- bettingGame.js
+++ bettingGame.js
@@ -1,35 +1,66 @@
-var cashAmount = 10;
+$(document).ready(function(){
 
-//check if cashAmount is less than 0
-
-function checkCash(cashAmount){
-  if (cashAmount <= 0){
-    return false;
-    console.log("Sorry you lost, better luck next time!")
-  };
-};
-
-
-
-
-function bet(){
-  while(cashAmount > 0){
-    var betAmount = prompt ("How much do you want to bet?");
-    var guess = prompt("Guess what the number is.");
+  var cashAmount = 100;
+  
+  $('form').submit(function(event){
+      
+    var betAmount = $('#amount').val();
+    var guess = $('#guess_number').val();
     var answer = Math.floor(Math.random() * 10 + 1);
 
     if (answer == guess){
-      cashAmount += betAmount;
-      alert("You win, you now have: $" + cashAmount);
+      cashAmount += parseInt(betAmount);
+      $('#correct_or_incorrect').text("correct");
     } else if (answer != guess + 1 && answer != guess - 1){
       cashAmount -= betAmount;
-      alert( "You lost, you now have: $" + cashAmount);
+      $('#correct_or_incorrect').text("incorrect");
     } else {
-      alert("You are one off from the answer, so you won't lose any money You have: $" + cashAmount);
+      $('#correct_or_incorrect').text("close but not the right answer");
     }
-  }
-    console.log("You lost");
-}
+
+    $('#winning').text(cashAmount);
+    $('#correct_answer').text(answer);
+
+    // document.getElementById('winning').innerHTML = cashAmount;
+    // document.getElementById('tomato').innerHTML = guess;
+    //prevents page from doing default action: refresh
+    if (event){
+      event.preventDefault();
+    }
 
 
-bet(); 
+    if (cashAmount < 0){
+      alert("You lose-ha!");
+      location.reload();
+    };
+
+  });
+
+
+});
+
+
+
+
+
+        // alert("You are one off from the answer, so you won't lose any money You have: $" + cashAmount);
+        // alert("You win, you now have: $" + cashAmount);
+    // console.log("You lost");
+// //check if cashAmount is less than 0
+
+// function checkCash(cashAmount){
+//   if (cashAmount <= 0){
+//     return false;
+//     console.log("Sorry you lost, better luck next time!")
+//   };
+// };
+
+// function bet(){
+//   while(cashAmount > 0){
+//     var betAmount = prompt ("How much do you want to bet?");
+//     var guess = prompt("Guess what the number is.");
+//   }
+// }
+
+
+// bet(); 
